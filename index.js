767f897aa5c59616dff372fb1832aa638cb8938a
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2020-03-31 12:46:00 +1300

    Added a few comments

--- index.js
+++ index.js
@@ -16,18 +16,22 @@ client.on("ready", () => {
 })
 
 client.on("message", msg => {
+    // Check for prefix
     if (!msg.content.startsWith(process.env.PREFIX) || msg.author.bot) return
 
     const args = msg.content.slice(process.env.PREFIX.length).split(/ +/)
     const commandName = args.shift().toLowerCase()
     
+    // Try getting the command by name, otherwise get it by alias
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
     
+    // Check if command exists
     if (!command) {
         msg.channel.send(`Unknown command \`${commandName}\``)
         return
     }
 
+    // Check for valid arguments if necessary
     if (command.args && !args.length) {
         msg.channel.send(`That command requires arguments.\nTry \`${process.env.PREFIX}${command.name} ${command.args}\``)
         return
